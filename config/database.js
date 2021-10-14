659c71de4344d80fae82b8537cd6c3538b110c52
Byoungcheon Lee <lbcsultan@gmail.com>
2021-10-14 13:24:50 +0900

    1st

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //database: 'mongodb://localhost:27017/meanauth',
+  database:
+    "mongodb+srv://prof_lee1:w4JOg4e0ddNPB9lV@cluster0.jdi63.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secret: "yoursupersecrets---flkjfdkfdghljkfhglskjdfhgkl",
+};
